0d6e9322b5051f3016ed94e817302e284ffa97e1
huangdandan <502513647@qq.com>
2020-12-07 09:31:26 +0800

    新建工程

---
+++ app/src/main/java/com/teach/aipinyin/MainActivity.java
@@ -0,0 +1,18 @@
+package com.teach.aipinyin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
